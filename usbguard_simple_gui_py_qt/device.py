6362dcbd034e86c31bcf7fbd558affd0d88ae618
Marco Nicola <marconicola@disroot.org>
2019-11-24 19:32:39 +0100

    Create Device dataclass

---
+++ usbguard_simple_gui_py_qt/device.py
@@ -0,0 +1,27 @@
+# USBGuard Simple GUI Py/Qt
+# Copyright (C) 2019  Marco Nicola
+#
+# This file is part of "USBGuard Simple GUI Py/Qt".
+#
+# "USBGuard Simple GUI Py/Qt" is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# "USBGuard Simple GUI Py/Qt" is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "USBGuard Simple GUI Py/Qt".  If not, see
+# <https://www.gnu.org/licenses/>.
+
+from dataclasses import dataclass
+from .rules import Rule
+
+
+@dataclass
+class Device:
+    device_id: int
+    rule: Rule
