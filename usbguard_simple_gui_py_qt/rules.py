bf6b012ff384a4f5fddd5660db570f26598fc1ea
Marco Nicola <marconicola@disroot.org>
2019-11-24 18:58:54 +0100

    Add new DeviceInterfaceType dataclass to rules

--- usbguard_simple_gui_py_qt/rules.py
+++ usbguard_simple_gui_py_qt/rules.py
@@ -77,3 +77,29 @@ class DeviceId:
     @staticmethod
     def _hex_repr(value: Optional[int]) -> str:
         return '*' if value is None else '%04x' % value
+
+
+@dataclass
+class DeviceInterfaceType:
+    iface_class: int
+    iface_subclass: Optional[int]
+    iface_protocol: Optional[int]
+
+    def __repr__(self) -> str:
+        return f'{self.cc}:{self.ss}:{self.pp}'
+
+    @property
+    def cc(self) -> str:
+        return '%02x' % self.iface_class
+
+    @property
+    def ss(self) -> str:
+        return self._hex_repr(self.iface_subclass)
+
+    @property
+    def pp(self) -> str:
+        return self._hex_repr(self.iface_protocol)
+
+    @staticmethod
+    def _hex_repr(value: Optional[int]) -> str:
+        return '*' if value is None else '%02x' % value
